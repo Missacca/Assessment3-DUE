bb28281dec871ba279c2b7e2d4ec3270bd33ee32
Missacca <2511994585@qq.com>
2023-10-14 19:01:57 +0800

    123
    123

---
+++ Assessment 3/Assets/Script/Page4.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Page4 : MonoBehaviour
+{
+    public void TS()
+    {
+        SceneManager.LoadScene("Page 4");
+    }
+}
---
+++ Assessment 3/Assets/Script/Page5.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Page5 : MonoBehaviour
+{
+    public void TS()
+    {
+        SceneManager.LoadScene("Page 5");
+    }
+}
